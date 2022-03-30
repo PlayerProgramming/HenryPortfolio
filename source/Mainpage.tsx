891f56053e6dbf63ff489e816ba8806a8ee412b8
PlayerOne <playerprogramming@gmail.com>
2022-03-30 02:19:27 -0700

    Added buttons for the general informations using "FlatView" and "TouchableOpacity"

--- source/Mainpage.tsx
+++ source/Mainpage.tsx
@@ -8,6 +8,9 @@ import {
   Image,
   Dimensions,
   Modal,
+  TouchableOpacity,
+  FlatList,
+  Alert,
 } from "react-native";
 import React from "react";
 
@@ -20,20 +23,39 @@ const Colors: any = {
   white: "#fff",
   greyish: "#a4a4a4",
 };
+
+const info: Array<any> = [
+  {
+    id: "bio",
+    title: "Bio",
+  },
+  {
+    id: "programming",
+    title: "Languages",
+  },
+  {
+    id: "project",
+    title: "Projects",
+  },
+  {
+    id: "Button4",
+    title: "Button4",
+  },
+];
 export default function Mainpage(props) {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#D1E1D9" }}>
       <ScrollView
         contentContainerStyle={{
           flexGrow: 1,
-
-          //   alignContent: "center",
-          //   justifyContent: "center",
         }}
       >
         <View
           style={{
-            flex: 0.5,
+            flex: 0.3,
+            flexDirection: "row",
+            margin: 10,
+            borderRadius: 50,
             backgroundColor: "#808080",
             alignItems: "center",
             justifyContent: "center",
@@ -43,15 +65,51 @@ export default function Mainpage(props) {
             style={styles.header}
             source={require("../assets/header_logo.jpg")}
           />
-          <Text>Henry Juhyeong Kang</Text>
-          <Text>Programming Name: PlayerOne</Text>
-          <Text>React Native Developer</Text>
+          <Text>
+            Henry Juhyeong Kang {"\n"}Programming Name: PlayerOne{"\n"}React
+            Native Developer
+          </Text>
         </View>
-        <View style={{ flex: 1, backgroundColor: "#4B4B4B" }}>
-          <Text>1111111111</Text>
+        <View
+          style={{
+            flex: 0.8,
+            backgroundColor: "#4B4B4B",
+            margin: 10,
+            borderRadius: 50,
+          }}
+        >
+          <FlatList
+            data={info}
+            numColumns={2}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  margin: 30,
+                  borderRadius: 25,
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+                onPress={() => Alert.alert("Hi")}
+              >
+                <Image
+                  source={require("../assets/header_logo.jpg")}
+                  style={{ width: "100%", height: "100%", borderRadius: 25 }}
+                />
+                <Text>{item.title}</Text>
+              </TouchableOpacity>
+            )}
+          />
         </View>
-        <View style={{ flex: 1, backgroundColor: "#c6c6c6" }}>
-          <Text>22222222</Text>
+        <View
+          style={{
+            flex: 0.15,
+            backgroundColor: "#c6c6c6",
+            margin: 10,
+            borderRadius: 50,
+          }}
+        >
+          <Text>Contact Information</Text>
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -67,8 +125,10 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   header: {
-    width: "50%",
+    width: "20%",
     height: "50%",
+    borderRadius: 1000,
+    margin: 10,
   },
 });
 
@@ -79,4 +139,19 @@ const styles = StyleSheet.create({
   backgroundColor="#00BCD4"
   translucent={true}
 />; */
+}
+{
+  /* <Text style={{ margin: 20 }}>
+            Hello, my name is Henry Juhyeong kang {"\n"}I graduated BCIT in
+            2020.
+            {"\n"}
+          </Text> */
+}
+{
+  /* <TouchableOpacity style={{ flex: 0.5, margin: 20, borderRadius: 25 }}>
+  <Image
+    source={require("../assets/header_logo.jpg")}
+    style={{ width: "100%", height: "100%", borderRadius: 25 }}
+  />
+</TouchableOpacity>; */
 }
