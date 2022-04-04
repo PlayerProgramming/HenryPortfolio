acc33fe4416d497c4da920ac0fadd96a71811dbe
PlayerOne <playerprogramming@gmail.com>
2022-04-04 05:30:58 -0700

    Finished filling in all the contents for each infos.

---
+++ source/data.tsx
@@ -0,0 +1,181 @@
+export const felanguage: Array<any> = [
+  {
+    name: "HTML",
+  },
+  {
+    name: "CSS",
+  },
+  {
+    name: "JavaScript",
+  },
+  {
+    name: "TypeScript",
+  },
+  {
+    name: "React",
+  },
+  {
+    name: "React Native",
+  },
+];
+export const belanguage: Array<any> = [
+  {
+    name: "Java",
+  },
+  {
+    name: "JavaScript",
+  },
+  {
+    name: "C++",
+  },
+  {
+    name: "Oracle SQL",
+  },
+  {
+    name: "Firebase",
+  },
+  {
+    name: "TypeScript",
+  },
+];
+export const gameDevelopment: Array<any> = [
+  {
+    name: "Unreal Engine",
+  },
+  {
+    name: "Unity",
+  },
+];
+export const mobileDevelopment: Array<any> = [
+  {
+    name: "Java",
+  },
+  {
+    name: "Android Studio",
+  },
+];
+
+export const projectContents = [
+  {
+    title: "NightLight",
+    subtitle: "[ 1st place, Safety Category in BCIT Contest]",
+    checkout: "https://github.com/PlayerProgramming/Night_LightV2",
+    content:
+      "- Collaborated with 3 classmates to develop an android app which displays " +
+      "the brightest path based on the amount of lamps on the streets using Java on " +
+      "Android Studio, Google Map API, Vancouver Open Data API \n\n" +
+      "- Designed visual mock-ups/prototypes from wire-frames and" +
+      "delivered final design production-ready assets for development \n\n" +
+      "- Implemented a function which sends a preset" +
+      "google map route of the user to contacts in emergency situations",
+  },
+  {
+    title: "Allegro",
+    subtitle: "[ 1st place, Creativity Category in BCIT Contest ]",
+    checkout: "https://github.com/PlayerProgramming/Allegro",
+    content:
+      "- Applied Agile in a group of 4, developed and delivered a game " +
+      "web application using JavaScript, Firebase,and MongoDB to host the web \n\n" +
+      "- Responsible for designing user pages, implementing a front-end" +
+      "Organize and led the team to confirm if the progresses are completed in a sufficient time",
+  },
+  {
+    title: "Lotto Random Number Generator",
+    subtitle: "[ Toy Project ]",
+    checkout: "https://github.com/PlayerProgramming/Lotto-RNG",
+    content:
+      "Created a simple mobile + web apps using Expo CLI to display random 6 numbers for LottoMax",
+  },
+  {
+    title: "Portfolio",
+    subtitle: "[The one you are looking at]",
+    checkout: "https://github.com/PlayerProgramming/HenryPortfolio",
+    content: "- Made it for my basic introductions using a bit of typescript",
+  },
+];
+
+export const interestContents = [
+  {
+    title: "Games",
+    image: "",
+    content:
+      "League of Legends, Enter the Gungeon, Fortnite, Rainbow Six Siege,\n" +
+      "TMI1: My League Of Legends highest tier was Master\n" +
+      "TMI2: I have streamd Fortnite and got 100 followers and 40 average views in '3' days.",
+  },
+  {
+    title: "Sports",
+    image: "",
+    content:
+      "I really love swimming the most and basketball. I currently do workout at least 5 days a week.",
+  },
+  {
+    title: "Typing",
+    image: "",
+    content: "The highest wpm I typed was 172 wpm on Monkeytype.com",
+  },
+];
+export const experienceContents = [
+  {
+    name: "Elite Education",
+    date: "2020 May ~ 2020 August",
+    details: "- Tutoring JavaScript and Java for students, ages 8 - 15",
+  },
+  {
+    name: "British Columbia Institute of Technology",
+    date: "2018 Spetember ~ 2020 June",
+    details:
+      "- Achieved Diploma in Computer System Technology Learned Object " +
+      "Oriented Programming, Algorithms, Web Development, Leaderships ",
+  },
+  {
+    name: "Canadian Mental Health Association, Volunteer",
+    date: "2016 Spetember ~ 2017 Feburary",
+    details:
+      "- Enhanced the scheduling of the facility. \nStrengthened the" +
+      "knowledge of the mental illness and the symptoms of it. \n" +
+      "- Pursued communicating and interacting with people who have mental" +
+      "illnesses. \n",
+  },
+  {
+    name: "Swift Current Aquatic Centre, Swim Instructor",
+    date: "2016 November ~ 2017 January",
+    details:
+      "- Prepared various swim courses and routiens efficiently for youth.\n" +
+      "- Directed the students to ensure the safety and rules. \nInformed" +
+      "the progresses of the students to their guardians.\n",
+  },
+  {
+    name: "Saskatchewan Comprehensive Highschool",
+    date: "2014 Spetember ~ 2018 June",
+    details: "Located in Swift Current, Saskatchewan ",
+  },
+];
+
+export const info: Array<any> = [
+  {
+    id: "Bio",
+    title: "Bio",
+    image: require("../assets/Bio.png"),
+  },
+  {
+    id: "Skills",
+    title: "Skills",
+    image: require("../assets/Skills.png"),
+  },
+  {
+    id: "Projects",
+    title: "Projects",
+    image: require("../assets/Projects.png"),
+  },
+  {
+    id: "Experiences",
+    title: "Experiences",
+    image: require("../assets/Experiences.png"),
+  },
+  {
+    id: "Interests",
+    title: "Interests",
+    image: require("../assets/Interests.png"),
+  },
+];
